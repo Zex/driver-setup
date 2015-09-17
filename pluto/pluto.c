a8a313be5df39a98c71a6cdc17af6fa4762625f6
Zex Li <top_zlynch@yahoo.com>
2015-09-17 16:29:33 +0800

    enter/leaveeeeeee

--- pluto/pluto.c
+++ pluto/pluto.c
@@ -1,6 +1,17 @@
-
+/** Pluto module
+ *
+ * Author: Zex Li <top_zlynch AT yahoo.com>
+ */
 #include <linux/kernel.h>
 #include <linux/modules.h>
 
+int init_module(void)
+{
+    printk(KERN_INFO "[Pluto] entering ...\n");
+}
 
+void cleanup_module(void)
+{
+    printk(KERN_INFO "[Pluto] leaving ...\n");
+}
 
