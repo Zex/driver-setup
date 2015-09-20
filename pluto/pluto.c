fb0eb0b5d905de240992cfb103c0103d89b2bb62
Zex Li <top_zlynch@yahoo.com>
2015-09-20 23:28:05 +0800

    add module info

--- pluto/pluto.c
+++ pluto/pluto.c
@@ -5,6 +5,16 @@
 #include <linux/kernel.h>
 #include <linux/modules.h>
 
+MODULE_LICENSE("MIT");
+MODULE_AUTHOR("Zex Li");
+MODULE_DESCRIPTION("pluto.c");
+
+static char *whom = "UNIVERSE";
+module_param(whom, charp, 0);
+
+static int howmany = 3;
+module_param(howmany, int, 0);
+
 int init_module(void)
 {
     printk(KERN_INFO "[Pluto] entering ...\n");
@@ -14,4 +24,8 @@ void cleanup_module(void)
 {
     printk(KERN_INFO "[Pluto] leaving ...\n");
 }
+
+module_init(init_module);
+module_exit(cleanup_module);
+
 
