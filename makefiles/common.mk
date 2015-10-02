e2bc700104eba9939d52675c6f04f8d68f8ea689
Zex Li <top_zlynch@yahoo.com>
2015-10-02 12:29:10 +0800

    add copyright

--- makefiles/common.mk
+++ makefiles/common.mk
@@ -1,4 +1,6 @@
-
+# common.mk - Common definition for makefiles
+# Author: Zex Li <top_zlynch AT yahoo.com>
+# Copyright 2015 Zex Li <top_zlynch AT yahoo.com>. All Rights Reserved
 
 export BUILD	:= $(PWD)/build
 export MKDIR	:= mkdir -p
